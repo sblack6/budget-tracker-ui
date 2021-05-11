6791c79518dacd7d3b6c8eb0714b4d3b02fd9d33
Sean Black <black.sean14@gmail.com>
2021-05-11 11:43:04 -0700

    Add home component and update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { BudgetHomeComponent } from './components/budget-home/budget-home.component';
 import { BudgetListComponent } from './components/budget-list/budget-list.component';
 import { EditBudgetComponent } from './components/edit-budget/edit-budget.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'budget', component: BudgetHomeComponent },
   { path: 'budget/:type/:date', component: BudgetListComponent },
   { path: 'budget/edit/:type/:date', component: EditBudgetComponent },
-  { path: '', redirectTo: 'budget', pathMatch: 'full' },
-  { path: '**', redirectTo: 'budget', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
